703c14bc717c59b4fffb597bb35adc3c132cc301
Adam Khalifa <akhalifa1982@gmail.com>
2022-10-20 01:41:10 +0300

    add tip calculation logic. set keyboard behaviour

--- we split/we split/ContentView.swift
+++ we split/we split/ContentView.swift
@@ -13,38 +13,57 @@ struct ContentView: View {
 		@State private var numberOfPeople = 2
 		@State private var tipPercentage = 15
 		@State private var tipPercentages = [15, 20, 30, 40]
+		@FocusState private var amountIsFocused: Bool
+
+		var totalPerPerson: Double {
+
+				let peopleCount = Double(numberOfPeople + 2)
+				let tipSelection = Double(tipPercentage)
+				let tipValue = checkAmount / 100 * tipSelection
+				let grandTotal = checkAmount + tipValue
+				let amountPerPerson = grandTotal / peopleCount
+
+				return amountPerPerson
+		}
 
 		var body: some View {
 				NavigationView {
-						Section {
-								Form {
-										Section{
-												TextField("amount", value: $checkAmount, format:
-																.currency(code: Locale.current.currencyCode ?? ("USD")))
-												.keyboardType(.decimalPad)
+						Form {
+								Section {
+										TextField("amount", value: $checkAmount, format:
+														.currency(code: Locale.current.currencyCode ?? "USD"))
+										.keyboardType(.decimalPad)
+										.focused($amountIsFocused)
 
-												Picker("number of people", selection: $numberOfPeople) {
-														ForEach(2..<10) {
-																Text("\($0)")
-														}
+										Picker("number of people", selection: $numberOfPeople) {
+												ForEach(2..<10) {
+														Text("\($0)")
 												}
 										}
-										Section {
-												Picker("Tip percentage", selection: $tipPercentage) {
-														ForEach(tipPercentages, id: \.self) {
-																Text($0, format: .percent)
-														}
-												}												.pickerStyle(.segmented)
-
-										} header: {
-												Text("pick the tip percentage you want to leave")
+								}
+								Section {
+										Picker("Tip percentage", selection: $tipPercentage) {
+												ForEach(tipPercentages, id: \.self) {
+														Text($0, format: .percent)
+												}
 										}
-										Section {
-												TextField("check amount", value: $checkAmount, format:
-																.currency(code: Locale.current.currencyCode ?? ("USD")))
+										.pickerStyle(.segmented)
+
+								} header: {
+										Text("pick the tip percentage you want to leave")
+								}
+								Section {
+										Text(totalPerPerson, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+								}
+						}
+						.navigationTitle("We split")
+						.toolbar{
+								ToolbarItemGroup(placement: .keyboard) {
+										Spacer()
+										Button("Done"){
+												amountIsFocused = false
 										}
 								}
-								.navigationTitle("We split")
 						}
 				}
 		}
