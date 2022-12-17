82eb78c89070e9c3daed115f61e513e635fd20f4
Adam <akhalifa1982@gmail.com>
2022-12-17 17:53:54 +0300

    rename variable

--- Commerce concept/Commerce concept/Modules/Top View/TopView.swift
+++ Commerce concept/Commerce concept/Modules/Top View/TopView.swift
@@ -17,17 +17,17 @@ struct TopView: View {
         VStack {
             ScrollView(.horizontal, showsIndicators: false){
                 LazyHGrid(rows: galleryItems, spacing: 23) {
-                    ForEach(items, id: \.self) { items in
+                    ForEach(items, id: \.self) { item in
                         VStack {
                             Button(action: {}) {
                                 HStack {
-                                    Image(items.image)
+                                    Image(item.image)
                                         .frame(width: 73, height: 73, alignment: .center)
                                         .foregroundColor(.white)
                                 }
                             }
                             .buttonStyle(MyButtonStyle())
-                            Text(items.image)
+                            Text(item.image)
                                 .foregroundColor(.white)
                         }
                     }
