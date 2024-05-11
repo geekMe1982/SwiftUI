dd8d28374f839c2107f32d45a753eb2004da624b
Adam Khalifa <70577640+geekMe1982@users.noreply.github.com>
2024-05-11 13:53:20 +0300

    init app

---
+++ NASA_API/Nasa_API/Nasa_APIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nasa_APIApp.swift
+//  Nasa_API
+//
+//  Created by Adam Khalifa on 11.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct Nasa_APIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
